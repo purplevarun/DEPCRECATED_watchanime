8f3d750a0020a78cac33973ec3ed3e05afd8d7b2
purplevarun <purplevarun@gmail.com>
2021-10-23 01:20:45 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+# -h for help
+# -n for name
+# -q for quality
+import sys
+arguments = sys.argv
+numOfArguments = len (sys.argv) - 1
+filename = arguments[0]
+arguments=arguments[1:]
+NAME = None
+QUALITY = None
+

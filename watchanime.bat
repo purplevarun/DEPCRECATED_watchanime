a996963539d582dd3ef0a129b64cef4f838b3e98
purplevarun <purplevarun@gmail.com>
2021-10-27 08:40:27 +0530

    7za added

--- watchanime.bat
+++ watchanime.bat
@@ -1,2 +1,16 @@
-python -m pip install --upgrade pip bs4 requests html5lib
-python main.py %*
+@echo off
+echo installation started && ^
+7za e mpv.7z && ^
+echo mpv player extracted successfully && ^
+python -m pip install --upgrade pip wheel virtualenv && ^
+virtualenv watchanime-env && ^
+watchanime-env\scripts\activate && ^
+watchanime-env\scripts\activate && ^
+cls && ^
+echo virtual environment created.... && ^
+python -m pip install --upgrade pip bs4 requests html5lib && ^
+cls && ^
+echo watchanime was successfully installed... && ^
+python main.py %* && ^
+deactivate && ^
+echo watchanime virtual environment deactivated
